fe9f7117ff454ec7e9e2b17978268ed580ca7f97
shelost <57425238+shelost@users.noreply.github.com>
2020-01-20 17:12:12 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shelost/hwsite.git master:master
+git push -f git@github.com:shelost/hwsite.git master:gh-pages
 
 cd -
